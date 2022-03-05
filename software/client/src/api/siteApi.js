e9b5b079884b4822f50a3946e9872536fde26203
Aniket Satish Mane <aniketsmd310@gmail.com>
2022-03-05 10:01:39 +0530

    done chages

--- software/client/src/api/siteApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: process.env.React_App_BACK_END_URL,
-  headers: {
-    "authorization": localStorage.getItem("token")? localStorage.getItem("token"):null,
-  }
-});
